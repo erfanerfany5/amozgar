4630fffc050dd5a0b9bf1994b83b0e1d81ca14c2
erfanerfany5 <erfanerfany5@gmail.com>
2016-12-30 20:38:59 +0330

    Add files via upload

---
+++ configuration.php
@@ -0,0 +1,70 @@
+<?php
+class JConfig {
+/* Site Settings */
+var $offline = '0';
+var $offline_message = 'این سایت برای به روز رسانی در دسترس نمی باشد.<br /> لطفا در ساعات آتی به سایت مراجعه کنید.';
+var $sitename = 'www.amozgar-rashidian.com';
+var $editor = 'tinymce';
+var $list_limit = '20';
+var $legacy = '0';
+/* Debug Settings */
+var $debug = '0';
+var $debug_lang = '0';
+/* Database Settings */
+var $dbtype = 'mysql';
+var $host = 'localhost';
+var $user = 'root';
+var $password = '2368';
+var $db = 'joomla';
+var $dbprefix = 'jos_';
+/* Server Settings */
+var $live_site = '';
+var $secret = 'jgeJ9d8FV7zwenm1';
+var $gzip = '0';
+var $error_reporting = '-1';
+var $helpurl = 'http://help.joomla.ir';
+var $xmlrpc_server = '0';
+var $ftp_host = '127.0.0.1';
+var $ftp_port = '21';
+var $ftp_user = '';
+var $ftp_pass = '';
+var $ftp_root = '';
+var $ftp_enable = '0';
+var $force_ssl = '0';
+/* Locale Settings */
+var $offset = '0';
+var $offset_user = '0';
+/* Mail Settings */
+var $mailer = 'mail';
+var $mailfrom = 'jet_leef@yahoo.com';
+var $fromname = 'www.amozgar-rashidian.com';
+var $sendmail = '/usr/sbin/sendmail';
+var $smtpauth = '0';
+var $smtpsecure = 'none';
+var $smtpport = '25';
+var $smtpuser = '';
+var $smtppass = '';
+var $smtphost = 'localhost';
+/* Cache Settings */
+var $caching = '0';
+var $cachetime = '15';
+var $cache_handler = 'file';
+/* Meta Settings */
+var $MetaDesc = 'جوملا! فارسی - سیستم مدیریت محتوا - پشتیبانی و توسعه توسط گروه نرم افزاری جوملا!';
+var $MetaKeys = 'جوملا!, جوملا';
+var $MetaTitle = '1';
+var $MetaAuthor = '1';
+/* SEO Settings */
+var $sef           = '0';
+var $sef_rewrite   = '0';
+var $sef_suffix    = '0';
+/* Feed Settings */
+var $feed_limit   = 10;
+var $feed_email   = 'author';
+var $log_path = 'C:\\xampp\\htdocs\\fa-IR_Joomla_Software_Group_full.1.5.14v8_www.ir4u.ir\\logs';
+var $tmp_path = 'C:\\xampp\\htdocs\\fa-IR_Joomla_Software_Group_full.1.5.14v8_www.ir4u.ir\\tmp';
+/* Session Setting */
+var $lifetime = '15';
+var $session_handler = 'database';
+}
+?>
